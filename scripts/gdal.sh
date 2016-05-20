a8f78f7d95baf9a4e55053c9b10c7d1c21e7a26a
jcradit <jcradit@trcsolutions.com>
2016-05-20 11:43:11 -0500

    added exit

--- scripts/gdal.sh
+++ scripts/gdal.sh
@@ -2,6 +2,7 @@ cd /opt
 if  [ -f "geoserver-2.8.1-gdal-plugin.zip"]
 then
   echo "already done"
+  exit 0
 else
   wget https://sourceforge.net/projects/geoserver/files/GeoServer/2.8.1/extensions/geoserver-2.8.1-gdal-plugin.zip
   unzip /opt/geoserver-2.8.1-gdal-plugin.zip -d /opt/geoserver-2.8.1-gdal-plugin
