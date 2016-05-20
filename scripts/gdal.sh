fdd6831f343f08d55255c128d6c96f44470ca307
jcradit <jcradit@trcsolutions.com>
2016-05-20 11:39:09 -0500

    check for file

--- scripts/gdal.sh
+++ scripts/gdal.sh
@@ -1,4 +1,9 @@
 cd /opt
-wget https://sourceforge.net/projects/geoserver/files/GeoServer/2.8.1/extensions/geoserver-2.8.1-gdal-plugin.zip
-unzip /opt/geoserver-2.8.1-gdal-plugin.zip -d /opt/geoserver-2.8.1-gdal-plugin
-cp /opt/geoserver-2.8.1-gdal-plugin/* /usr/share/opengeo/geoserver/WEB-INF/lib/
+if  [ -f "geoserver-2.8.1-gdal-plugin.zip"]
+then
+  echo "already done"
+else
+  wget https://sourceforge.net/projects/geoserver/files/GeoServer/2.8.1/extensions/geoserver-2.8.1-gdal-plugin.zip
+  unzip /opt/geoserver-2.8.1-gdal-plugin.zip -d /opt/geoserver-2.8.1-gdal-plugin
+  cp /opt/geoserver-2.8.1-gdal-plugin/* /usr/share/opengeo/geoserver/WEB-INF/lib/
+fi
